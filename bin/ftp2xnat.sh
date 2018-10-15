6760a4a95f3116d5f5be751c4bf5cc92180f77c8
Nikola Bogetic <nikolabogetic@gmail.com>
2018-10-15 14:44:20 -0400

    ftp2xnat fixed for conda

--- bin/ftp2xnat.sh
+++ bin/ftp2xnat.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 PROJECT=$1
+PYTHON=/opt/anaconda3/envs/datman-env/bin/python3.6
 
 if [ $# -eq 0 ] ; then
     echo "Usage: ./ftp2xnat.sh <PROJECTID>"
     exit 1
 fi
 
-conda activate datman-env &&
-python dm_sftp.py -d $PROJECT &&
-python write_manifest.py -d $PROJECT &&
-python dm_link.py -d $PROJECT &&
-python dm_xnat_upload.py -d $PROJECT &&
-conda deactivate &&
-echo "###########################"
+. /opt/anaconda3/envs/datman-env/etc/conda/activate.d/env_vars.sh &&
+$PYTHON /usr/local/bin/datman/bin/dm_sftp.py -d $PROJECT &&
+$PYTHON /usr/local/bin/datman/bin/write_manifest.py -d $PROJECT &&
+$PYTHON /usr/local/bin/datman/bin/dm_link.py -d $PROJECT &&
+$PYTHON /usr/local/bin/datman/bin/dm_xnat_upload.py -d $PROJECT &&
+. /opt/anaconda3/envs/datman-env/etc/conda/deactivate.d/env_vars.sh &&
+echo "###########################" &&
 echo "##         DONE          ##"
 echo "###########################"
-
